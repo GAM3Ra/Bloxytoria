67604d6f18321b5aeb6f9b3c9a2cfb35508a9b62
GAM3Ra <zaeemiq6@gmail.com>
2026-02-13 13:03:48 +0000

    Create main.lua as the entry point for the Bloxytoria game engine

---
+++ main.lua
@@ -0,0 +1,16 @@
+-- main.lua
+
+-- Entry point for the Bloxytoria game engine
+
+function love.load()
+    print("Welcome to Bloxytoria!")
+    -- Initialization code goes here
+end
+
+function love.update(dt)
+    -- Update game state
+end
+
+function love.draw()
+    -- Render the game
+end
